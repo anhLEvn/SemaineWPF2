6c177f3510e3ca72bd7a04910ed637fdfed5c5de
LE Thi Van Anh <57898934+anhLEvn@users.noreply.github.com>
2021-09-29 14:07:20 +0200

    Add project files.

---
+++ DataBinding_ViewModel/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using DataBinding_ViewModel.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBinding_ViewModel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window 
+
+    {
+        private AdherentViewModel adherentViewModel = new AdherentViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = adherentViewModel; 
+        }
+
+        
+    }
+}
---
+++ DataBinding_ViewModel/ViewModel/AdherentViewModel.cs
@@ -0,0 +1,29 @@
+﻿using DataBinding_ViewModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBinding_ViewModel.ViewModel
+{
+    class AdherentViewModel
+    {
+        //public List<Adherent> ListeAdherents = new List<Adherent>();
+        public List<Adherent> ListeAdherents { get; set; }
+
+        private DBAdherentsEntities db = new DBAdherentsEntities(); 
+
+        public AdherentViewModel()
+        {
+            if(ListeAdherents == null)
+            {
+                ListeAdherents = new List<Adherent>();
+                ListeAdherents = db.Adherents.ToList();
+            }
+            
+        }
+    }
+   
+
+}
